b7299654a427a65cc09bbc84b0ffd3429a272452
kdl <testqa.kdl@gmail.com>
2015-09-23 15:25:08 +0300

    add_api_cTypes_lists

---
+++ tests/vpn/api_realtyAddFlatCest.php
@@ -0,0 +1,18 @@
+<?php
+use \VpnTester;
+
+class api_realtyAddFlatCest
+{
+    public function _before(VpnTester $I)
+    {
+    }
+
+    public function _after(VpnTester $I)
+    {
+    }
+
+    // tests
+    public function tryToTest(VpnTester $I)
+    {
+    }
+}
